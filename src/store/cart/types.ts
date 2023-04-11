0617353f05b35a992d8ae5074bcea881c55557bf
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-11 20:25:20 -0300

    Feat: declarando types interfaces

---
+++ src/store/cart/types.ts
@@ -0,0 +1,18 @@
+
+
+
+export interface IProduct {
+    id: number,
+    title: string,
+    price: number,
+}
+
+export interface ICartItem {
+    product: IProduct,
+    quantity: number
+}
+
+export interface ICartState {
+    items: ICartItem[],
+    failedStockCheck: number[];
+}
