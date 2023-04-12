5e6b88352f8864091912b47c564679bfa47d9f57
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-11 21:05:37 -0300

    Feat: componente de catalog

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,12 +1,31 @@
 
+import { useEffect, useState } from "react"
+import { IProduct } from "../../store/cart/types"
+import api from "../../service/api"
+import { CatalogItem } from "./CatalogItem"
+
 
 
 export function Catalog (){
+    const [catalog, setCatalog] = useState<IProduct[]>([])
 
+    useEffect(()=> {
+        api.get('/products').then(response =>{
+            setCatalog(response.data)
+        })
+    }, [])
+
+    
+    
 
     return (
-        <>
+        <main>
             <h1> Catalog </h1>
-        </>
+            {
+                catalog.map(product => (
+                    <CatalogItem key={product.id} product={product} />
+                ))
+            }
+        </main>
     )
 }
