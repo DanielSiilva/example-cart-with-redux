71c80e076814367da63d272f0a9f5b42e17a14cc
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-11 21:39:17 -0300

    Feat: componente cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,16 +1,35 @@
 import { useSelector } from "react-redux"
+import { IState } from "../../store"
+import { ICartItem } from "../../store/cart/types"
 
 
 
 export function Cart (){
-    const state = useSelector(state => state)
-    console.log(state);
+    const cart = useSelector<IState, ICartItem[]>(state => state.cart.items)
+   
     
 
     return (
-        <>
-            <h1> Cart </h1>
-            <p></p>
-        </>
+        <table>
+            <thead>
+                <tr>
+                    <th>Produto</th>
+                    <th>Preço</th>
+                    <th>Quantidade</th>
+                    <th>Subtotal</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {cart.map(item =>(
+                    <tr key={item.product.id}>
+                        <td>{item.product.title}</td>
+                        <td>{item.product.price}</td>
+                        <td>{item.quantity}</td>
+                        <td>{(item.quantity * item.product.price).toFixed(2)}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     )
 }
